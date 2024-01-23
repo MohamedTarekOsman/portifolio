1aec2a1e00646790c18408a13ee682a7e38bd134
MohamedTarek <mt469063@gmail.com>
2024-01-23 23:57:27 +0200

    final commit

--- src/index.js
+++ src/index.js
@@ -11,9 +11,6 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  // <Provider store={Store}>
-  //   <App/>
-  // </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
